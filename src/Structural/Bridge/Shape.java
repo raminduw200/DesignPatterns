86864ddf5bc453754eb351f4b010be41707b7be1
raminduw200 <raminduw200@gmail.com>
2022-09-16 12:02:16 +0530

    Add Bridge pattern

---
+++ src/Structural/Bridge/Shape.java
@@ -0,0 +1,11 @@
+package Structural.Bridge;
+
+public abstract class Shape {
+    protected DrawAPI drawAPI;
+
+    protected Shape(DrawAPI drawAPI){
+        this.drawAPI = drawAPI;
+    }
+
+    public abstract void draw();
+}
